2dc507be5733beb954fa7bd76803110d55ffa647
Anuj Manekar <anuj.manekar@technia.com>
2026-02-15 00:49:37 +0530

    Data types

---
+++ src/feb/ex_15022026/Lab005Datatypes.java
@@ -0,0 +1,25 @@
+package feb.ex_15022026;
+//Data types
+public class Lab005Datatypes {
+    public static void main(String[] args) {
+
+        //Primitive data types
+        byte b =100;
+        System.out.println(b);
+
+        char system = 'A';
+        System.out.println(system);
+
+        float f = 123.45f;
+        System.out.println(f);
+
+        double d = 123.456;
+        System.out.println(d);
+
+        int i = 123;
+        System.out.println(i);
+
+        long l = 123;
+        System.out.println(l);
+    }
+}
---
+++ src/feb/ex_15022026/lab004.java
@@ -0,0 +1,9 @@
+package feb.ex_15022026;
+
+public class lab004 {
+    public static void main(String[] args) {
+        boolean _ = true;
+        boolean anuj123 = false;
+        System.out.println(anuj123);
+    }
+}
